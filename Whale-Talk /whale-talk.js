0f12675174976e9046039fec6c688d63f9c3643c
rob-vault <14255092+rob-vault@users.noreply.github.com>
2024-04-29 02:21:53 -0400

    Create whale-talk.js

---
+++ Whale-Talk /whale-talk.js
@@ -0,0 +1,22 @@
+let input = 'turpentine and turtles';
+
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+let resultArray = [];
+
+for (let i = 0; i < input.length; i++) {
+  if (input[i] === 'e' || input[i] === 'u') {
+    resultArray.push(input[i]);
+  };
+  for (let j = 0; j < vowels.length; j++) {
+    if (input[i] === vowels[j]) {
+      resultArray.push(input[i]);
+    };
+  };
+};
+
+let resultString = function () {
+  resultArray = resultArray.join('');
+  console.log(resultArray.toUpperCase());
+};
+
+resultString();
